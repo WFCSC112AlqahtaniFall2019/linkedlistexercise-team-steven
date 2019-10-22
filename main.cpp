8fd551ee809f517a5a574c11f34315eee9e075cb
Steven <parrsm18@wfu.edu>
2019-10-22 18:11:24 -0400

    Coded remaining tasks

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
     int x;
     Node* head= new Node();
-    Node* current= head;
+    Node* current = head;
     cout<<"enter a list of integers and -1 to stop"<<endl;
     cin>>x;
     //populate the linked list with at least 7 nodes
@@ -17,7 +17,6 @@ int main() {
        current->InsertAfter(n);
        current=n; //current=current->getNext;
        cin>>x;
-
     }
 
     //print the nodes of the linked list
@@ -28,14 +27,112 @@ int main() {
         current=current->getNext();
     }
 
+    cout << endl;
+
     //1) find the node that has the minimum value
+    int min;
+    Node * next;
+    current=head->getNext();
+    min = head->getValue();
+    while (current!=nullptr && current->getNext()!= nullptr){
+        next = current->getNext();
+        if (current->getValue() > next->getValue()){
+            min = next->getValue();
+        }
+        current = current->getNext();
+    }
+    cout << "Minimum value is: " << min << endl;
+    cout << endl;
 
     //2) remove the 4th node from the list
 
+    current = head->getNext();
+    Node * previous = head->getNext();
+    current = current->getNext();
+    current = current->getNext();
+    previous = previous->getNext();
+    current = current->getNext();
+    previous = previous->getNext();
+    previous->setNext(current->getNext());
+    current->setNext(nullptr);
+    delete(current);
+
+    //Prints list
+
+    cout << "List with 4th element removed:" << endl;
+    current=head->getNext();
+    while(current!=nullptr)
+    {
+        current->printNode();
+        current=current->getNext();
+    }
+    cout << endl;
+
+
     //3) remove the last node
 
+    current = head->getNext();
+    previous = head->getNext();
+    current = current->getNext();
+
+    while(current != nullptr && current->getNext()!= nullptr){
+        current = current->getNext();
+        previous = previous->getNext();
+    }
+
+    previous->setNext(nullptr);
+    delete(current);
+
+    //Prints list
+    cout << "List with last element removed:" << endl;
+
+    current=head->getNext();
+    while(current!=nullptr)
+    {
+        current->printNode();
+        current=current->getNext();
+    }
+    cout << endl;
+
     //4) remove the first node
+    current = head->getNext();
+    head->setNext(current->getNext());
+    current->setNext(nullptr);
+    delete(current);
+
+    //Prints list
+    cout << "List with first element removed:" << endl;
+
+    current=head->getNext();
+    while(current!=nullptr)
+    {
+        current->printNode();
+        current=current->getNext();
+    }
+    cout << endl;
+
 
     //6) delete all nodes
+    current = head->getNext();
+    while(head != nullptr && head->getNext()!= nullptr) {
+        current = head->getNext();
+        head->setNext(current->getNext());
+        current->setNext(nullptr);
+        delete (current);
+    }
+
+    //Prints list
+    cout << "List with all elements removed:" << endl;
+
+    current=head->getNext();
+    while(current!=nullptr)
+    {
+        current->printNode();
+        current=current->getNext();
+    }
+    cout << endl;
+
+    cout << "Finished deleting all nodes" << endl;
+
     return 0;
 }
